3917c11a6513e6c51fb75b61d5ea6e7fdb4ef75f
Winy <danielamanzanorangel@gmail.com>
2025-11-04 09:22:21 -0600

    Subiendo nuevo proyecto al repositorio

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./features/auth/login/login.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'convam-cliente';
+}
